b9a64b538138192ac47db91993f0d38ed2331016
Yudai Fujiwara <ptr.yudai+dev@gmail.com>
2022-03-15 21:41:39 +0900

    eigorian

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -77,103 +77,106 @@ const Index: NextPage<IndexPageProps> = ({
 }: IndexPageProps) => {
   const { data: ctf } = useCTF(fallbackCTF);
   return (
-    <Stack mt={5}>
-      <Box maxW="container.sm" mx="auto">
-        <Box maxW="3xs" mx="auto">
-          <Image unoptimized={true} src={zer0pts_svg} />
-        </Box>
-        <Center fontSize="4xl">zer0pts CTF 2022</Center>
+      <Stack mt={5}>
+          <Box maxW="container.sm" mx="auto">
+              <Box maxW="3xs" mx="auto">
+                  <Image unoptimized={true} src={zer0pts_svg} />
+              </Box>
+              <Center fontSize="4xl">zer0pts CTF 2022</Center>
 
-        {ctf && (
-          <>
-            <Center>
-              {dateFormat(ctf.start_at)} - {dateFormat(ctf.end_at)}
-            </Center>
-            <Center>
-              <CountDown ctf={ctf} />
-            </Center>
-          </>
-        )}
-      </Box>
+              {ctf && (
+                  <>
+                      <Center>
+                          {dateFormat(ctf.start_at)} - {dateFormat(ctf.end_at)}
+                      </Center>
+                      <Center>
+                          <CountDown ctf={ctf} />
+                      </Center>
+                  </>
+              )}
+          </Box>
 
-      <Text fontSize="xl" mt={10}>
-        [ About ]
-      </Text>
-      <Text pl={4}>
-        Welcome to zer0pts CTF 2022! <br />
-        zer0pts CTF is a jeopardy-style CTF.
-        <br />
-        We provide many fun challenges of varying difficulty and categories,
-        none of them requiring any guessing skill.
-      </Text>
+          <Text fontSize="xl" mt={10}>
+              [ About ]
+          </Text>
+          <Text pl={4}>
+              Welcome to zer0pts CTF 2022! <br />
+              zer0pts CTF is a jeopardy-style CTF.
+              <br />
+              We provide many fun challenges of varying difficulty and categories,
+              and none of them require any guessing skills.
+          </Text>
 
-      <Text fontSize="xl" mt={10}>
-        [ Contact ]
-      </Text>
-      <Text pl={4}>Discord: <a href="https://discord.gg/3QrDP2sMYd">https://discord.gg/3QrDP2sMYd</a></Text>
+          <Text fontSize="xl" mt={10}>
+              [ Contact ]
+          </Text>
+          <Text pl={4}>Discord: <a href="https://discord.gg/3QrDP2sMYd">https://discord.gg/3QrDP2sMYd</a></Text>
 
-      <Text fontSize="xl" mt={10}>
-        [ Prizes ]
-      </Text>
-      <Stack pl={4}>
-        <Text fontWeight="bold">1st place</Text>
-        <UnorderedList pl={4}>
-          <ListItem>UOUO FISH LIFE</ListItem>
-        </UnorderedList>
-      </Stack>
+          <Text fontSize="xl" mt={10}>
+              [ Prizes ]
+          </Text>
+          <Stack pl={4}>
+              <UnorderedList pl={4}>
+                  <ListItem><Text fontWeight="bold">1st: 800 USD</Text></ListItem>
+                  <ListItem><Text fontWeight="bold">2nd: 500 USD</Text></ListItem>
+                  <ListItem><Text fontWeight="bold">3rd: 300 USD</Text></ListItem>
+                  <ListItem><Text fontWeight="bold">4th: 200 USD</Text></ListItem>
+                  <ListItem><Text fontWeight="bold">5th: 200 USD</Text></ListItem>
+              </UnorderedList>
+          </Stack>
 
-      <Text fontSize="xl" mt={4}>
-        [ Rules ]
-      </Text>
-      <Text pl={4}>
-        <UnorderedList>
-          <ListItem>Your team can be of any size.</ListItem>
-          <ListItem>
-            Anyone is allowed to participate: no restriction on age or
-            nationality.
-          </ListItem>
-          <ListItem>
-            Your position on the scoreboard depends on 2 things: 1) your total
-            number of points (higher is better); 2) the timestamp of your last
-            solved challenge (lower is better).
-          </ListItem>
-          <ListItem>
-            The survey challenge is special: it does award you some points, but
-            it doesn't update your "last solved challenge" timestamp. You can't
-            get ahead simply by solving the survey faster.
-          </ListItem>
-          <ListItem>
-            You can't brute-force flags. If you submit 5 incorrect flags in a
-            short succession, the flag submission form will get locked for 5
-            minutes.
-          </ListItem>
-          <ListItem>One person can participate in only one team.</ListItem>
-          <ListItem>
-            Sharing solutions, hints or flags with other teams during the
-            competition is strictly forbidden.
-          </ListItem>
-          <ListItem>You are not allowed to attack the scoreserver.</ListItem>
-          <ListItem>You are not allowed to attack other teams.</ListItem>
-          <ListItem>
-            You are not allowed to have multiple accounts. If you can't log in
-            to your account, please contact us on Discord.
-          </ListItem>
-          <ListItem>
-            We reserve the right to ban and disqualify any team that chooses to
-            break any of these rules.
-          </ListItem>
-          <ListItem>
-            The flag format is{" "}
-            <Code variant="solid" colorScheme="gray">
-              {"zer0pts\\{[\\x20-\\x7e]+\\}"}
-            </Code>
+          <Text fontSize="xl" mt={4}>
+              [ Rules ]
+          </Text>
+          <Text pl={4}>
+              <UnorderedList>
+                  <ListItem>No limit on your team size.</ListItem>
+                  <ListItem>
+                      Anyone can participate in this CTF:
+                      no restriction on your age or nationality.
+                  </ListItem>
+                  <ListItem>
+                      Your rank on the scoreboard depends on: 1) your total
+                      number of points (higher is better); 2) the timestamp
+                      of your last solved challenge (erlier is better).
+                  </ListItem>
+                  <ListItem>
+                      The survey challenge is special: it does award you some points, but
+                      it doesn't update your "last solved challenge" timestamp. You can't
+                      get ahead simply by solving the survey faster.
+                  </ListItem>
+                  <ListItem>
+                      You can't brute-force the flags. If you submit 5 incorrect flags in a
+                      short succession, the flag submission form will get locked for 5
+                      minutes.
+                  </ListItem>
+                  <ListItem>One person can participate in only one team.</ListItem>
+                  <ListItem>
+                      Sharing solutions, hints, or flags with other teams during the
+                      competition is strictly forbidden.
+                  </ListItem>
+                  <ListItem>You are not allowed to attack the scoreserver.</ListItem>
+                  <ListItem>You are not allowed to attack other teams.</ListItem>
+                  <ListItem>
+                      You are not allowed to have multiple accounts. If you can't log in
+                      to your account, please contact us on Discord.
+                  </ListItem>
+                  <ListItem>
+                      We reserve the right to ban and disqualify any teams breaking
+                      any of these rules.
+                  </ListItem>
+                  <ListItem>
+                      The flag format is{" "}
+                      <Code variant="solid" colorScheme="gray">
+                          {"zer0pts\\{[\\x20-\\x7e]+\\}"}
+                      </Code>
             , unless specified otherwise.
-          </ListItem>
-          <ListItem>Most importantly: good luck and have fun!</ListItem>
-        </UnorderedList>
-      </Text>
-    </Stack>
-  );
+                  </ListItem>
+                  <ListItem>Most importantly: good luck and have fun!</ListItem>
+              </UnorderedList>
+          </Text>
+      </Stack>
+);
 };
 
 export const getStaticProps: GetStaticProps<IndexPageProps> = async () => {

f158ee88f214de77be802e145f44c10caf4f98ad
theoremoon <theoldmoon0602@gmail.com>
2022-07-26 00:06:11 +0900

    save

--- scoreserver/repository/message.go
+++ scoreserver/repository/message.go
@@ -1,30 +1,4 @@
 package repository
 
-import (
-	"github.com/theoremoon/kosenctfx/scoreserver/model"
-	"golang.org/x/xerrors"
-)
-
 type MessageRepository interface {
-	GetMessage(key string) (*model.Message, error)
-	SetMessage(key, value string) error
-}
-
-func (r *repository) GetMessage(key string) (*model.Message, error) {
-
-	var m model.Message
-	if err := r.db.Where("key = ?", key).First(&m).Error; err != nil {
-		return nil, xerrors.Errorf(": %w", err)
-	}
-	return &m, nil
-}
-
-func (r *repository) SetMessage(key, value string) error {
-	if err := r.db.Create(&model.Message{
-		Key:   key,
-		Value: value,
-	}).Error; err != nil {
-		return xerrors.Errorf(": %w", err)
-	}
-	return nil
 }
